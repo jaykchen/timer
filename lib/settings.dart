ac794ff12d6c3e6a6a94cf34c88fadc76e89a979
jaykchen <jaykchen@gmail.com>
2021-12-04 15:43:01 -0500

    minor changes in settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -171,22 +171,3 @@ class SettingsButton extends StatelessWidget {
     );
   }
 }
-
-
-
-Create an empty Git repository or reinitialize an existing one
-
-git init
-Add file contents to the index
-
-git add .
-Commit changes to the repository
-
-git commit -m "initial commit"
-Add a remote
-
-git remote add origin <remote-repo-url>
-Show URLs of remote repositories
-git remote -v
-Push the changes to remote repo
-git push -f origin master
